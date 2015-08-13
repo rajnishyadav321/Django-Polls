a69302b0872eaca9780d13b6296b5f5c4bf975f3
rajnishyadav321 <rajnishyadav321@gmail.com>
2015-08-13 13:12:26 +0530

    Adding Project to repository

---
+++ test_env/test_project_new/polls/models.py
@@ -0,0 +1,30 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    def __str__(self):              # __unicode__ on Python 2
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    search_fields = ['question_text']
+
+
+class Choice(models.Model):
+    def __str__(self):              # __unicode__ on Python 2
+        return self.choice_text
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
